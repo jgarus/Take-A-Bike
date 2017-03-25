04e93992940d62eab6986ef6be25e3390e986abd
dgreen88 <dgreen88@uncc.edu>
2017-03-25 01:21:11 -0400

    push2

--- app/src/main/java/com/take_a_bikev2/MapsActivity.java
+++ app/src/main/java/com/take_a_bikev2/MapsActivity.java
@@ -1,10 +1,12 @@
 package com.take_a_bikev2;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -21,7 +23,15 @@ import java.util.List;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationSource.OnLocationChangedListener mListener;
     private List<Marker> originMarkers = new ArrayList<>();
+
+    public static Integer numberSU = 3;
+    public static Integer numberWW = 2;
+    public static Integer numberSAC = 6;
+    public static Integer numberAT = 4;
+    public static Integer numberBR = 9;
+    public static Integer numberSM = 8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,29 +44,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
+    public void onLocationChanged(Location location)
+    {
+        if( mListener != null )
+        {
+            mListener.onLocationChanged( location );
+
+            //Move the camera to the user's location and zoom in!
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 12.0f));
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         LatLng studentunion = new LatLng(35.308607, -80.733736);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(studentunion, 300));
-        originMarkers.add(mMap.addMarker(new MarkerOptions().title("Student Union").position(studentunion)));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(studentunion, 17.0f));
+        //mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 12.0f));
+        originMarkers.add(mMap.addMarker(new MarkerOptions().title("Student Union: " + numberSU).position(studentunion)));
 
         LatLng woodword = new LatLng(35.307520, -80.735719);
-        originMarkers.add(mMap.addMarker(new MarkerOptions().title("Woodword hall").position(woodword)));
+        originMarkers.add(mMap.addMarker(new MarkerOptions().title("Woodword hall: " + numberWW).position(woodword)));
 
         LatLng StudentActivityCenter = new LatLng(35.3066644, -80.7366597);
-        originMarkers.add(mMap.addMarker(new MarkerOptions().title("Student Activity Center").position(StudentActivityCenter)));
+        originMarkers.add(mMap.addMarker(new MarkerOptions().title("Student Activity Center: " + numberSAC).position(StudentActivityCenter)));
 
 
         LatLng AtkinsLibrary = new LatLng(35.3056584, -80.7344607);
-        originMarkers.add(mMap.addMarker(new MarkerOptions().title("Atkins Library").position(AtkinsLibrary)));
+        originMarkers.add(mMap.addMarker(new MarkerOptions().title("Atkins Library: " + numberAT).position(AtkinsLibrary)));
 
 
         LatLng Burson = new LatLng(35.3062627, -80.7330765);
-        originMarkers.add(mMap.addMarker(new MarkerOptions().title("Burson").position(Burson)));
+        originMarkers.add(mMap.addMarker(new MarkerOptions().title("Burson: " + numberBR).position(Burson)));
 
         LatLng smith = new LatLng(35.3065323, -80.7331744);
-        originMarkers.add(mMap.addMarker(new MarkerOptions().title("Smith").position(smith)));
+        originMarkers.add(mMap.addMarker(new MarkerOptions().title("Smith: " + numberSM).position(smith)));
 
 
     }
