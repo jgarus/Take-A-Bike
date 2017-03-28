22b64615941c75e087b3b2cd9042c772ec0be772
dgreen88 <dgreen88@uncc.edu>
2017-03-27 23:28:51 -0400

    push3, firebase auth working

--- app/src/main/java/com/take_a_bikev2/LoginActivity.java
+++ app/src/main/java/com/take_a_bikev2/LoginActivity.java
@@ -3,11 +3,20 @@ package com.take_a_bikev2;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.regex.Pattern;
 
@@ -20,30 +29,58 @@ public class LoginActivity extends AppCompatActivity{
     static EditText _emailText;
     static EditText _passwordText;
     static Button _loginButton;
-
-    //email regex
     Pattern emailpattern = Pattern.compile("^[A-Za-z].*?@uncc\\.edu$");
+    private FirebaseAuth auth;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         _emailText = (EditText) findViewById(R.id.input_email);
-        _passwordText = (EditText)findViewById(R.id.input_password);
+        _passwordText = (EditText) findViewById(R.id.input_password);
         _loginButton = (Button) findViewById(R.id.btn_login);
+        auth = FirebaseAuth.getInstance(); //Firebase
 
         _loginButton.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
-                validate();//when button is clicked; call login() method
+                String email = _emailText.getText().toString();
+                final String password = _passwordText.getText().toString();
+
+                //validates email and password
+                if (email.isEmpty() || !emailpattern.matcher(email).matches()) {
+                    _emailText.setError("Please enter a valid UNCC email address.");
+                    return;
+                }
+                if (password.isEmpty() || password.length() < 8 || password.length() > 15 ) {
+                    _passwordText.setError("Password must be between 8 and 15 characters");
+                    return;
+                }
+
+                //authenticate user
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                if (!task.isSuccessful()) {
+                                        Toast.makeText(getBaseContext(), "Login FAILED", Toast.LENGTH_LONG).show();
+                                } else {
+                                    login();
+                                }
+                            }
+                        });
             }
-
         });
-
     }
-
+    
     public void login() {
 
         _loginButton.setEnabled(false);
@@ -54,19 +91,14 @@ public class LoginActivity extends AppCompatActivity{
         progressDialog.setMessage("Authenticating...");
         progressDialog.show();
 
-        // TODO: Implement your own authentication logic here.
-
         new android.os.Handler().postDelayed(
                 new Runnable() {
                     public void run() {
-                        // On complete call either onLoginSuccess or onLoginFailed
                         onLoginSuccess();
-                        // onLoginFailed();
                         progressDialog.dismiss();
                     }
                 }, 1250);
     }
-
 
     public void onLoginSuccess() {
         Toast.makeText(getBaseContext(), "Login Successful", Toast.LENGTH_LONG).show();
@@ -75,34 +107,6 @@ public class LoginActivity extends AppCompatActivity{
         Intent myIntent = new Intent(LoginActivity.this, MapsActivity.class);
         LoginActivity.this.startActivity(myIntent);
         finish();
-    }
-
-    public void onLoginFailed() {
-        Toast.makeText(getBaseContext(), "Login failed. Please use a school email", Toast.LENGTH_LONG).show();
-
-        _loginButton.setEnabled(true);
-    }
-
-    public void validate() {
-
-        //grabs credentials from user and stores them in String variables
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
-
-        //validates email and password
-        if (email.isEmpty() || !emailpattern.matcher(email).matches()) {
-            _emailText.setError("Please enter a valid UNCC email address.");
-            onLoginFailed();
-        } else {
-            _emailText.setError(null);
-            if (password.isEmpty() || password.length() < 8 || password.length() > 15 ) {
-                _passwordText.setError("Password must be between 8 and 15 characters");
-                onLoginFailed();
-            } else {
-                _passwordText.setError(null);
-                login();
-            }
-        }
     }
 
 }
